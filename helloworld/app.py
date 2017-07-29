111480e78539501a0b66245843346e510feb1874
Robert J. Berger <rberger@ibd.com>
2017-07-28 17:08:09 -0700

    Demo being able to talk to consul from another container

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,10 +1,16 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import consul
+
+c = consul.Consul(host='consul')
+c.kv.put('mykey', 'myvalue')
 
 def hello_world(request):
     print('Incoming request')
-    return Response('<body><h1>Hello World!</h1></body>')
+    index, data = c.kv.get('mykey')
+    str = '{0} value: {1}'.format(data["Key"], data["Value"])
+    return Response('<body><h1>Hello World!<br>' + str + '</h1></body>')
 
 if __name__ == '__main__':
     config = Configurator()
